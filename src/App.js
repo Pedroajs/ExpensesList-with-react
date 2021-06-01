bb432659e62d375bd692abbe8aacd8ae9b5c6021
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-06-01 11:56:48 -0300

    Novo compnent - form para modificar a tabela

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './Components/Expenses/Expenses';
+import NewExpense from './Components/NewExpense/NewExpense'
 
 function App() {
   const expenses = [
@@ -25,7 +26,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
       <Expenses items={expenses} />      
     </div>
   );
