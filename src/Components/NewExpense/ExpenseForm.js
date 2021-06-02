8927783220f2fce34e1938816da3b89bd9768f90
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-06-02 10:40:11 -0300

    lógica para modificar a entrada de dados usando múltiplos estados

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -1,23 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './ExpenseForm.css'
 
+
 function ExpenseForm(){
+    const [enteredTitle, setTitle] = useState('');
+    const [enteredAmount, setAmount] = useState('');
+    const [enteredDate, setDate] = useState('');
+
+
+    const titleChangeHandler = event => setTitle(event.target.value)
+    
+    const amountChangeHandler = event => setAmount(event.target.value)
+
+    const dateChangeHandler = event => setDate(event.target.value)
+
     return(
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label >Title</label>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <label >Amount</label>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" onChange={amountChangeHandler}  min="0.01" step="0.01" />
                 </div>
 
                 <div className="new-expense__control">
                     <label>Date</label>
-                    <input type="date" min="2019-01-01"  max="2022-12-31"/>
+                    <input type="date" onChange={dateChangeHandler} min="2019-01-01"  max="2022-12-31"/>
                 </div>
             </div>
 
